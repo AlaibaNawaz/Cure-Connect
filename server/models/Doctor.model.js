c047dc65eab91b10cd20b043834eb12e487b469e
AfnanHussain10 <afnan.h5050@gmail.com>
2025-05-08 01:15:18 +0500

    removed index on location

--- server/models/Doctor.model.js
+++ server/models/Doctor.model.js
@@ -52,9 +52,6 @@ const DoctorSchema = new mongoose.Schema({
   }
 });
 
-// Add geospatial index for location-based queries
-DoctorSchema.index({ location: '2dsphere' });
-
 const Doctor = User.discriminator('Doctor', DoctorSchema);
 
 export default Doctor;
